49056ca2bdb6386dc013338ff316792a8b92523f
extrateres260 <167340651+extrateres260@users.noreply.github.com>
2025-02-27 21:39:16 +0800

    Update Lab01-Q1.cpp

--- Lab01/Lab01-Q1.cpp
+++ Lab01/Lab01-Q1.cpp
@@ -1,1 +1,61 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        int Count = 0;
+
+        bool isincreasing = true;
+        bool isdecreasing = true;
+
+        // Two assignments (isincreasing and isdecreasing) : 2 operations
+        count++;
+        count++;
+
+        // Loop initialization : 1 operation
+        count++;
+
+        for (int i = 1; i < nums.size(); i++) {
+            // Loop comparison : 1 operation
+            count++;
+
+            // Accessing Array twice : 2 operations
+            count++;
+            count++;
+
+            // Comparison of elements (nums[i] < nums[i - 1]) : 1 operation
+            count++;
+
+            // Assignment of isincreasing : 1 operation
+            count++;
+
+            if (nums[i] < nums[i - 1]) { // if the precedent number is greater to the one after, it is not increasing
+                isincreasing = false;
+            }
+
+            // Accessing Array twice : 2 operation
+            count++;
+            count++;
+
+            // Comparison of elements (nums[i] > nums[i - 1]) : 1 operation
+            count++;
+
+            // Assignment of isdecreasing : 1 operation
+            count++;
+
+            if (nums[i] > nums[i - 1]) {   // if the following number is greater than the precedent, it is not decreasing
+                isdecreasing = false;
+            }
+        }
+
+        // Increment i++ : 1 operation
+        count++;
+
+        // Return operation : 1 operation
+        count++
+
+        cout << "Total operations: " << count << endl;
+
+        return isincreasing || isdecreasing; // display either the array if increasing or decreasing
+    }
+};
+
 
