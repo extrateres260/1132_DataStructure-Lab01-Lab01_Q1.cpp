c0c15fe9934c029eac8f761f4a0bea7180ac608c
extrateres260 <167340651+extrateres260@users.noreply.github.com>
2025-03-05 20:33:47 +0800

    Update Lab02_Q1.cpp

--- Lab02/Lab02_Q1.cpp
+++ Lab02/Lab02_Q1.cpp
@@ -1,5 +1,5 @@
-Class solution{
-  Public:
+class Solution {
+  public:
      int findLucky(vector<int>& arr) {
      // I sort the array first
      sort(arr.begin(), arr.end());
@@ -9,7 +9,7 @@ Class solution{
      for ( int i = 0; i < arr.size(); i++) {
        int freq = 0;
        for (int j = 0; j< arr.size(); j++) { 
-           if(arr[j] = arr[i]) {  // if an integer is equal to the previous one, its frequence grows by 1
+           if(arr[j] == arr[i]) {  // if an integer is equal to the previous one, its frequence grows by 1
              freq++;
            }
        }
@@ -19,9 +19,10 @@ Class solution{
          }
 
          // While looping through the array, if the next integer is equal to the previous one, just skip it
-         while( i+1 < arr.size() && arr[i] == arr[i+1] {
+         while( i+1 < arr.size() && arr[i] == arr[i+1]) {
               i++;
          }
        }
-     retun lucky_integer; // return the lucky integer
+     return lucky_integer; // return the lucky integer
   }
+};
